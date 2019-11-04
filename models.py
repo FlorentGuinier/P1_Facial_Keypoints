1357b484f124bbaa9bc62422998635afa07f39d2
Florent Guinier <florent@unity3d.com>
2019-11-04 08:59:39 +0100

    Add model definition for keypoint detection
    Input image will be 100x100 for performance reason.
    
    Summary of the network:
    Conv2D 5x5 16 kernels + relu + pool2 2x2 + batchnorm2d
    Conv2D 3x3 32 kernels + relu + pool2 2x2 + batchnorm2d
    Conv2D 3x3 64 kernels + relu + pool2 2x2 + batchnorm2d
    Conv2D 3x3 128 kernels + relu + pool2 2x2 + batchnorm2d
    Flatten
    Dense 1024 neurons + batchnorm1d
    Dense 256 neurons + batchnorm1d
    Dense 68*2 (ie 68 * x,y keypoints)

--- models.py
+++ models.py
@@ -1,37 +1,58 @@
-## TODO: define the convolutional neural network architecture
-
 import torch
-from torch.autograd import Variable
 import torch.nn as nn
 import torch.nn.functional as F
-# can use the below import should you choose to initialize the weights of your Net
-import torch.nn.init as I
-
-
+ 
 class Net(nn.Module):
 
     def __init__(self):
         super(Net, self).__init__()
         
-        ## TODO: Define all the layers of this CNN, the only requirements are:
-        ## 1. This network takes in a square (same width and height), grayscale image as input
-        ## 2. It ends with a linear layer that represents the keypoints
-        ## it's suggested that you make this last layer output 136 values, 2 for each of the 68 keypoint (x, y) pairs
+        # -- conv1 (input = 1x100x100) --
+        self.conv1 = nn.Conv2d( 1,  16, 5) # 1x100x100 -> 16x96x96
+        self.pool = nn.MaxPool2d(2, 2)     #  16x96x96 -> 16x48x48
+        self.bnconv1 = nn.BatchNorm2d(16)
         
-        # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
-        # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        # -- conv2 --
+        self.conv2 = nn.Conv2d(16,  32, 3) #  16x48x48 -> 32x46x46
+        #self.pool = nn.MaxPool2d(2, 2)    #  32x46x46 -> 32x23x23
+        self.bnconv2 = nn.BatchNorm2d(32)
         
-        ## Note that among the layers to add, consider including:
-        # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
+        # -- conv3 --
+        self.conv3 = nn.Conv2d(32,  64, 3) #  32x23x23 -> 64x21x21
+        #self.pool = nn.MaxPool2d(2, 2)    #  64x21x21 -> 64x10x10
+        self.bnconv3 = nn.BatchNorm2d(64)
         
-
+        # -- conv4 --
+        self.conv4 = nn.Conv2d(64, 128, 3) #  64x10x10 -> 128x8x8
+        #self.pool = nn.MaxPool2d(2, 2)    #  128x8x8  -> 128x4x4
+        self.bnconv4 = nn.BatchNorm2d(128)
         
+        # -- flatten --                    # 128x4x4   -> 2048
+                                           
+        # -- fc1 --
+        self.fc1 = nn.Linear(128*4*4, 1024)# 2048      -> 1024
+        self.bncfc1 = nn.BatchNorm1d(1024)
+        
+        # -- fc2 --
+        self.fc2 = nn.Linear(1024, 256)    # 2048      -> 256
+        self.bncfc2 = nn.BatchNorm1d(256)
+        
+        # -- fc3 (output = 136 ie 68*2)--
+        self.fc3 = nn.Linear(256, 68*2)   # 256       -> 136
+       
     def forward(self, x):
-        ## TODO: Define the feedforward behavior of this model
-        ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
-        
-        
-        # a modified x, having gone through all the layers of your model, should be returned
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.bnconv1(x)
+        x = self.pool(F.relu(self.conv2(x)))
+        x = self.bnconv2(x)
+        x = self.pool(F.relu(self.conv3(x)))
+        x = self.bnconv3(x)
+        x = self.pool(F.relu(self.conv4(x)))
+        x = self.bnconv4(x)
+        x = x.view(x.shape[0],-1)
+        x = F.relu(self.fc1(x))
+        x = self.bncfc1(x)
+        x = F.relu(self.fc2(x))
+        x = self.bncfc2(x)
+        x = self.fc3(x)
         return x
